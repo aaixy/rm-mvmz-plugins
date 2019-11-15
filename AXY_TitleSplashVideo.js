4601baf071735b68f2e11438e0c4e443070373f3
aaixy <54528229+aaixy@users.noreply.github.com>
2019-11-15 13:21:31 +0800

    1.02
    add: Closure;

--- AXY_TitleSplashVideo.js
+++ AXY_TitleSplashVideo.js
@@ -1,11 +1,11 @@
 //=============================================================================
 // A XueYu Plugins - Title Splash Video
 // AXY_TitleSplashVideo.js
-// Version: 1.01
+// Version: 1.02
 // License: MIT
 //=============================================================================
 /*:
- * @plugindesc v1.01 Display Splash Video before the Title Screen.
+ * @plugindesc v1.02 Display Splash Video before the Title Screen.
  * @author A XueYu Plugins
  * 
  * @help
@@ -14,8 +14,10 @@
  * to your boot page before the title screen.
  * 
  * changelog
+ * 1.02 2019.11.15
+ * add: Closure;
  * 1.01 2019.11.14
- * add: param: notice and notice color; this depends on AXY_Toast.js
+ * add: param: notice and notice color; This is depends on AXY_Toast.js
  * 1.00 2019.11.13
  * first release.
  * 
@@ -146,151 +148,153 @@ Object.keys(AXY.TitleSplashVideo.Parameters).forEach(function (key) {
 });
 
 // Main
-AXY.TitleSplashVideo.Alias.Scene_Boot_Display_Splash = Scene_Boot.prototype.start
-Scene_Boot.prototype.start = function () {
-	if (!DataManager.isBattleTest() && !DataManager.isEventTest()) {
-		SceneManager.push(AXY_TitleSplashVideo_Scene_Splash_Screen);
-		return
-	}
-	AXY.TitleSplashVideo.Alias.Scene_Boot_Display_Splash.call(this);
-};
-
-function AXY_TitleSplashVideo_Scene_Splash_Screen() {
-	this.initialize.apply(this, arguments);
-}
-AXY_TitleSplashVideo_Scene_Splash_Screen.prototype = Object.create(Scene_Base.prototype);
-AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.constructor = AXY_TitleSplashVideo_Scene_Splash_Screen;
-
-AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.initialize = function () {
-	Scene_Base.prototype.initialize.call(this);
-};
-
-AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.create = function () {
-	Scene_Base.prototype.create.call(this);
-	this._splashData = [0, 0, Math.max(AXY.TitleSplashVideo.Param.SplashDuration, 1), Math.max(AXY.TitleSplashVideo.Param.SplashFadeSpeed, 1)];
-	this._splashSprite = [];
-
-	this.videoName = 'movies/' + AXY.TitleSplashVideo.Param.videoName + Game_Interpreter.prototype.videoFileExt();
-	this.imageName = 'img/' + AXY.TitleSplashVideo.Param.imageName + '.png';
-	if (Decrypter.hasEncryptedImages) {
-		this.imageName = Decrypter.extToEncryptExt(this.imageName);
-	}
-
-	this._texture = PIXI.Texture.from(this.videoName);
-	//console.log(this._texture);
-
-	this._videoSource = this._texture.baseTexture.source;
-	//console.log(this._videoSource);
-	if (AXY.TitleSplashVideo.Param.loop == true) {
-		this._videoSource.loop = true;
-	} else {
-		this._videoSource.loop = false;
-	}
-	if (AXY.TitleSplashVideo.Param.muted == true) {
-		this._videoSource.muted = true;
-	} else {
-		this._videoSource.muted = false;
-	}
-	if (this.imageName.trim() != '') {
-		this._videoSource.poster = this.imageName;
-	}
-	var _that = this;
-	this._videoSource.addEventListener('ended', function (event) {
-		_that.terminate();
-	})
-	//this._videoSource.onended = this.terminate();
-
-	//this._videoSource.setAttribute('playsinline', '');
-	//this._videoSource.setAttribute('x5-video-player-type', 'h5');
-	//this._videoSource.setAttribute('x5-video-orientation', 'landscape');
-	//this._videoSource.setAttribute('x5-video-player-fullscreen', 'true');
-	//this._videoSource.setAttribute('controls', '');
-	//this._videoSource.setAttribute('x5-playsinline', '');
-	//this._videoSource.setAttribute('webkit-playsinline', '');
-	//this._videoSource.setAttribute('x-webkit-airplay', 'allow');
-	//this._videoSource.setAttribute('preload', 'auto');
-
-	this._spriteVideo = new PIXI.Sprite(this._texture);
-	this._spriteVideo.width = AXY.TitleSplashVideo.Param.width;
-	this._spriteVideo.height = AXY.TitleSplashVideo.Param.height;
-	this._spriteVideo.x = AXY.TitleSplashVideo.Param.x;
-	this._spriteVideo.y = AXY.TitleSplashVideo.Param.y;
-
-	//push sprite to array;
-	this._splashSprite.push(this._spriteVideo);
-	//console.log(this._spriteVideo);
-
-	this.addChild(this._spriteVideo);
-
-	//this.refresh_splash_screen();
-	if (AXY.TitleSplashVideo.Param.notice && typeof $.toaster == 'function') {
-		$.toaster({
-			message: AXY.TitleSplashVideo.Param.notice,
-			color: AXY.TitleSplashVideo.Param.noticeColor
-		});
-	}
-};
-
-AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.refresh_splash_screen = function () {
-	if (this._splashData[0] >= this._splashSprite.length) {
-		this.terminate();
+(function () {
+	AXY.TitleSplashVideo.Alias.Scene_Boot_Display_Splash = Scene_Boot.prototype.start
+	Scene_Boot.prototype.start = function () {
+		if (!DataManager.isBattleTest() && !DataManager.isEventTest()) {
+			SceneManager.push(AXY_TitleSplashVideo_Scene_Splash_Screen);
+			return
+		}
+		AXY.TitleSplashVideo.Alias.Scene_Boot_Display_Splash.call(this);
 	};
-	//this._titleSplashSprite.bitmap = this._spriteVideo.bitmap;
-	this._spriteVideo.opacity = 0;
-	this._splashData[0] += 1;
-	this._splashData[1] = this._splashData[2];
-};
 
-AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.start = function () {
-	Scene_Base.prototype.start.call(this);
-	this.startFadeIn(this.fadeSpeed(), false);
-};
-
-AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.update = function () {
-	Scene_Base.prototype.update.call(this);
-	//this.refresh_splash_screen();
-	if (Input.isTriggered("ok") || TouchInput.isTriggered()) {
-		this.terminate();
+	function AXY_TitleSplashVideo_Scene_Splash_Screen() {
+		this.initialize.apply(this, arguments);
 	}
-}
+	AXY_TitleSplashVideo_Scene_Splash_Screen.prototype = Object.create(Scene_Base.prototype);
+	AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.constructor = AXY_TitleSplashVideo_Scene_Splash_Screen;
 
-AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.terminate = function () {
-	Scene_Base.prototype.terminate.call(this);
-	//console.log(this);
-	//console.log(this._videoSource.paused);
-	if (!this._videoSource.paused) {
-		this.pauseVideo();
-		//console.log('111');
+	AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.initialize = function () {
+		Scene_Base.prototype.initialize.call(this);
+	};
+
+	AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.create = function () {
+		Scene_Base.prototype.create.call(this);
+		this._splashData = [0, 0, Math.max(AXY.TitleSplashVideo.Param.SplashDuration, 1), Math.max(AXY.TitleSplashVideo.Param.SplashFadeSpeed, 1)];
+		this._splashSprite = [];
+
+		this.videoName = 'movies/' + AXY.TitleSplashVideo.Param.videoName + Game_Interpreter.prototype.videoFileExt();
+		this.imageName = 'img/' + AXY.TitleSplashVideo.Param.imageName + '.png';
+		if (Decrypter.hasEncryptedImages) {
+			this.imageName = Decrypter.extToEncryptExt(this.imageName);
+		}
+
+		this._texture = PIXI.Texture.from(this.videoName);
+		//console.log(this._texture);
+
+		this._videoSource = this._texture.baseTexture.source;
+		//console.log(this._videoSource);
+		if (AXY.TitleSplashVideo.Param.loop == true) {
+			this._videoSource.loop = true;
+		} else {
+			this._videoSource.loop = false;
+		}
+		if (AXY.TitleSplashVideo.Param.muted == true) {
+			this._videoSource.muted = true;
+		} else {
+			this._videoSource.muted = false;
+		}
+		if (this.imageName.trim() != '') {
+			this._videoSource.poster = this.imageName;
+		}
+		var _that = this;
+		this._videoSource.addEventListener('ended', function (event) {
+			_that.terminate();
+		})
+		//this._videoSource.onended = this.terminate();
+
+		//this._videoSource.setAttribute('playsinline', '');
+		//this._videoSource.setAttribute('x5-video-player-type', 'h5');
+		//this._videoSource.setAttribute('x5-video-orientation', 'landscape');
+		//this._videoSource.setAttribute('x5-video-player-fullscreen', 'true');
+		//this._videoSource.setAttribute('controls', '');
+		//this._videoSource.setAttribute('x5-playsinline', '');
+		//this._videoSource.setAttribute('webkit-playsinline', '');
+		//this._videoSource.setAttribute('x-webkit-airplay', 'allow');
+		//this._videoSource.setAttribute('preload', 'auto');
+
+		this._spriteVideo = new PIXI.Sprite(this._texture);
+		this._spriteVideo.width = AXY.TitleSplashVideo.Param.width;
+		this._spriteVideo.height = AXY.TitleSplashVideo.Param.height;
+		this._spriteVideo.x = AXY.TitleSplashVideo.Param.x;
+		this._spriteVideo.y = AXY.TitleSplashVideo.Param.y;
+
+		//push sprite to array;
+		this._splashSprite.push(this._spriteVideo);
+		//console.log(this._spriteVideo);
+
+		this.addChild(this._spriteVideo);
+
+		//this.refresh_splash_screen();
+		if (AXY.TitleSplashVideo.Param.notice && typeof $.toaster == 'function') {
+			$.toaster({
+				message: AXY.TitleSplashVideo.Param.notice,
+				color: AXY.TitleSplashVideo.Param.noticeColor
+			});
+		}
+	};
+
+	AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.refresh_splash_screen = function () {
+		if (this._splashData[0] >= this._splashSprite.length) {
+			this.terminate();
+		};
+		//this._titleSplashSprite.bitmap = this._spriteVideo.bitmap;
+		this._spriteVideo.opacity = 0;
+		this._splashData[0] += 1;
+		this._splashData[1] = this._splashData[2];
+	};
+
+	AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.start = function () {
+		Scene_Base.prototype.start.call(this);
+		this.startFadeIn(this.fadeSpeed(), false);
+	};
+
+	AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.update = function () {
+		Scene_Base.prototype.update.call(this);
+		//this.refresh_splash_screen();
+		if (Input.isTriggered("ok") || TouchInput.isTriggered()) {
+			this.terminate();
+		}
 	}
 
-	//this.videoTitle.removeVideo();
-	SceneManager.snapForBackground();
-	if (typeof XY_TitleSplash_Scene_Splash_Screen === "function") {
-		SceneManager.goto(XY_TitleSplash_Scene_Splash_Screen);
+	AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.terminate = function () {
+		Scene_Base.prototype.terminate.call(this);
+		//console.log(this);
+		//console.log(this._videoSource.paused);
+		if (!this._videoSource.paused) {
+			this.pauseVideo();
+			//console.log('111');
+		}
+
+		//this.videoTitle.removeVideo();
+		SceneManager.snapForBackground();
+		if (typeof XY_TitleSplash_Scene_Splash_Screen === "function") {
+			SceneManager.goto(XY_TitleSplash_Scene_Splash_Screen);
+			//SceneManager.pop();
+		} else {
+			DataManager.setupNewGame();
+			SceneManager.goto(Scene_Title);
+			Window_TitleCommand.initCommandPosition();
+		}
+		//SceneManager.push();
 		//SceneManager.pop();
-	} else {
-		DataManager.setupNewGame();
-		SceneManager.goto(Scene_Title);
-		Window_TitleCommand.initCommandPosition();
-	}
-	//SceneManager.push();
-	//SceneManager.pop();
 
 
-	return;
-};
+		return;
+	};
 
-AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.pauseVideo = function () {
-	this._videoSource.pause();
-};
+	AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.pauseVideo = function () {
+		this._videoSource.pause();
+	};
 
-AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.playVideo = function () {
-	this._videoSource.play();
-};
+	AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.playVideo = function () {
+		this._videoSource.play();
+	};
 
-AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.removeVideo = function () {
-	this._videoSource.pause();
-	this._videoSource.src = null;
-	this._videoSource.removeAttribute('src');
-	this._videoSource.load();
-};
+	AXY_TitleSplashVideo_Scene_Splash_Screen.prototype.removeVideo = function () {
+		this._videoSource.pause();
+		this._videoSource.src = null;
+		this._videoSource.removeAttribute('src');
+		this._videoSource.load();
+	};
+})();
