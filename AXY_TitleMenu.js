c3fd9d731fcb62a42d6256683a35fbb5e80000f8
aaixy <54528229+aaixy@users.noreply.github.com>
2020-03-01 16:32:08 +0800

    Add files via upload

--- AXY_TitleMenu.js
+++ AXY_TitleMenu.js
@@ -1,12 +1,12 @@
 //=============================================================================
-// XueYu Plugins - Title Menu
+// A XueYu Plugins - Title Menu
 // AXY_TitleMenu.js
-// Version: 1.0
-// License: BSD
+// Version: 1.01
+// License: MIT
 //=============================================================================
- /*:
- * @plugindesc v1.00 Display Multiple Menu in Title Screen.
- * @author XueYu Plugins
+/*:
+ * @plugindesc v1.01 Display Multiple Menu in Title Screen.
+ * @author A XueYu Plugins
  *
  * @param DisplayJumpToMap
  * @desc Display Jump To Map? true/false
@@ -32,6 +32,19 @@
  * @param ExitText
  * @desc The name will be display in title screen's command window.
  * @default Exit
+ * 
+ * @param OverrideContinue
+ * @desc Override Continue with jump to map? true/false
+ * @default false
+ *
+ * @param OverrideContinueMapId
+ * @desc The Id of map that you will jump to. 1,2,3...etc.
+ * @default 1
+ * 
+ * @param OverrideContinueSwitchId
+ * @desc The id of Switch that you will use to after jump to map.
+ * 1,2,3...etc. Set to 0 to disable.
+ * @default 0
  *
  * @param MaxCols
  * @desc The title screen command window MaxCols.
@@ -68,6 +81,13 @@
  * Introduction
  * This plugin allows you to place Multiple Menu command
  * to your title screen.
+ * 
+ * changelog
+ * 1.01 2020.3.1
+ * add: OverrideContinue;
+ * modify: switch License from BSD to MIT;
+ * 1.00 2016
+ * first release;
  */
 
 // Imported
@@ -80,6 +100,7 @@ AXY.TitleMenu = AXY.TitleMenu || {};
 
 AXY.TitleMenu.Parameters = PluginManager.parameters('AXY_TitleMenu');
 AXY.TitleMenu.Param = AXY.TitleMenu.Param || {};
+AXY.TitleMenu.Value = AXY.TitleMenu.Value || {};
 
 // Display Multiple Menu
 AXY.TitleMenu.Param.DisplayJumpToMap = AXY.TitleMenu.Parameters['DisplayJumpToMap'].toLowerCase() === 'true';
@@ -88,6 +109,9 @@ AXY.TitleMenu.Param.JumpToMapId = parseInt(AXY.TitleMenu.Parameters['JumpToMapId
 AXY.TitleMenu.Param.JumpToMapSwitchId = parseInt(AXY.TitleMenu.Parameters['JumpToMapSwitchId']);
 AXY.TitleMenu.Param.DisplayExit = AXY.TitleMenu.Parameters['DisplayExit'].toLowerCase() === 'true';
 AXY.TitleMenu.Param.ExitText = String(AXY.TitleMenu.Parameters['ExitText']);
+AXY.TitleMenu.Param.OverrideContinue = AXY.TitleMenu.Parameters['OverrideContinue'].toLowerCase() === 'true';
+AXY.TitleMenu.Param.OverrideContinueMapId = parseInt(AXY.TitleMenu.Parameters['OverrideContinueMapId']);
+AXY.TitleMenu.Param.OverrideContinueSwitchId = parseInt(AXY.TitleMenu.Parameters['OverrideContinueSwitchId']);
 AXY.TitleMenu.Param.MaxCols = parseInt(AXY.TitleMenu.Parameters['MaxCols']);
 AXY.TitleMenu.Param.WindowWidth = parseInt(AXY.TitleMenu.Parameters['WindowWidth']);
 AXY.TitleMenu.Param.X = String(AXY.TitleMenu.Parameters['X']);
@@ -96,75 +120,107 @@ AXY.TitleMenu.Param.WindowSkin = String(AXY.TitleMenu.Parameters['WindowSkin']);
 AXY.TitleMenu.Param.Align = String(AXY.TitleMenu.Parameters['Align']);
 AXY.TitleMenu.Param.TextColor = String(AXY.TitleMenu.Parameters['TextColor']);
 
+AXY.TitleMenu.Value.CurrentMapId = 0;
+AXY.TitleMenu.Value.CurrentSwitchId = 0;
+
 // Main
 // Window_TitleCommand
 AXY.TitleMenu.Window_TitleCommand_makeCommandList = Window_TitleCommand.prototype.makeCommandList;
-Window_TitleCommand.prototype.makeCommandList = function() {
+Window_TitleCommand.prototype.makeCommandList = function () {
     AXY.TitleMenu.Window_TitleCommand_makeCommandList.call(this);
-	this.AXY_TitleMenu_addCommand();
+    this.AXY_TitleMenu_addCommand();
 };
 
-Window_TitleCommand.prototype.AXY_TitleMenu_addCommand = function() {
-	if(AXY.TitleMenu.Param.DisplayJumpToMap){
-		this.addCommand(AXY.TitleMenu.Param.JumpToMapText, 'JumpToMap');
-	}
-	if(AXY.TitleMenu.Param.DisplayExit){
-		this.addCommand(AXY.TitleMenu.Param.ExitText, 'Exit');
-	}
-	
+Window_TitleCommand.prototype.AXY_TitleMenu_addCommand = function () {
+    if (AXY.TitleMenu.Param.DisplayJumpToMap) {
+        this.addCommand(AXY.TitleMenu.Param.JumpToMapText, 'JumpToMap');
+    }
+    if (AXY.TitleMenu.Param.DisplayExit) {
+        this.addCommand(AXY.TitleMenu.Param.ExitText, 'Exit');
+    }
+    // if (1) {
+    //     this.addCommand("common event", 'CommonEventMenu');
+    // }
+
 };
 
-Window_TitleCommand.prototype.maxCols = function() {
+Window_TitleCommand.prototype.maxCols = function () {
     return AXY.TitleMenu.Param.MaxCols;
 };
 
-Window_TitleCommand.prototype.windowWidth = function() {
+Window_TitleCommand.prototype.windowWidth = function () {
     return AXY.TitleMenu.Param.WindowWidth;
 };
 
-Window_TitleCommand.prototype.updatePlacement = function() {
+Window_TitleCommand.prototype.updatePlacement = function () {
     this.x = eval(AXY.TitleMenu.Param.X);
     this.y = eval(AXY.TitleMenu.Param.Y);
 };
 
-Window_TitleCommand.prototype.loadWindowskin = function() {
-this.windowskin = ImageManager.loadSystem(AXY.TitleMenu.Param.WindowSkin);
+Window_TitleCommand.prototype.loadWindowskin = function () {
+    this.windowskin = ImageManager.loadSystem(AXY.TitleMenu.Param.WindowSkin);
 };
 
-Window_TitleCommand.prototype.itemTextAlign = function() {
+Window_TitleCommand.prototype.itemTextAlign = function () {
     return AXY.TitleMenu.Param.Align;
 };
 
-Window_TitleCommand.prototype.resetTextColor = function() {
-	/*this.outlineColor = 'rgba(255, 0, 0, 0.5)';
+Window_TitleCommand.prototype.resetTextColor = function () {
+    /*this.outlineColor = 'rgba(255, 0, 0, 0.5)';
     this.outlineWidth = 4;*/
-	this.changeTextColor(AXY.TitleMenu.Param.TextColor);
+    this.changeTextColor(AXY.TitleMenu.Param.TextColor);
 };
 
 // Scene_Base
 AXY.TitleMenu.Scene_Title_createCommandWindow = Scene_Title.prototype.createCommandWindow;
-Scene_Title.prototype.createCommandWindow = function() {
+Scene_Title.prototype.createCommandWindow = function () {
     AXY.TitleMenu.Scene_Title_createCommandWindow.call(this);
-	
-	if(AXY.TitleMenu.Param.DisplayJumpToMap){
-		this._commandWindow.setHandler('JumpToMap',  this.commandJumpToMap.bind(this));
-	}
-	if(AXY.TitleMenu.Param.DisplayExit){
-		this._commandWindow.setHandler('Exit',  this.commandExit.bind(this));
-	}
+
+    if (AXY.TitleMenu.Param.DisplayJumpToMap) {
+        this._commandWindow.setHandler('JumpToMap', this.commandJumpToMap.bind(this));
+    }
+    if (AXY.TitleMenu.Param.DisplayExit) {
+        this._commandWindow.setHandler('Exit', this.commandExit.bind(this));
+    }
+    // if (1) {
+    //     this._commandWindow.setHandler('CommonEventMenu', this.commandCommonEventMenu.bind(this));
+    // }
 };
 
-Scene_Title.prototype.commandExit = function() {
+Scene_Title.prototype.commandExit = function () {
     this._commandWindow.close();
     SceneManager.exit();
 };
 
-Scene_Title.prototype.commandJumpToMap = function() {
+Scene_Title.prototype.commandJumpToMap = function () {
     DataManager.setupNewGame();
     this._commandWindow.close();
     this.fadeOutAll();
+    AXY.TitleMenu.Value.CurrentMapId = AXY.TitleMenu.Param.JumpToMapId;
+    AXY.TitleMenu.Value.CurrentSwitchId = AXY.TitleMenu.Param.JumpToMapSwitchId;
     SceneManager.goto(Scene_JumpToMap_Map);
 };
+
+// Scene_Title.prototype.commandCommonEventMenu = function () {
+//     DataManager.setupNewGame();
+//     this._commandWindow.close();
+//     this.fadeOutAll();
+//     SceneManager.goto(Scene_JumpToMap_Map);
+//     $gameTemp.reserveCommonEvent(151);
+// };
+
+if (AXY.TitleMenu.Param.OverrideContinue) {
+    Scene_Title.prototype.commandContinue = function () {
+        DataManager.setupNewGame();
+        this._commandWindow.close();
+        this.fadeOutAll();
+        //SceneManager.push(Scene_Load);
+        AXY.TitleMenu.Value.CurrentMapId = AXY.TitleMenu.Param.OverrideContinueMapId;
+        AXY.TitleMenu.Value.CurrentSwitchId = AXY.TitleMenu.Param.OverrideContinueSwitchId;
+        SceneManager.goto(Scene_JumpToMap_Map);
+        //$gameTemp.reserveCommonEvent(151);
+    };
+}
 
 // Scene_JumpToMap_Map
 // The scene class of the map screen.
@@ -175,7 +231,7 @@ function Scene_JumpToMap_Map() {
 Scene_JumpToMap_Map.prototype = Object.create(Scene_Map.prototype);
 Scene_JumpToMap_Map.prototype.constructor = Scene_JumpToMap_Map;
 
-Scene_JumpToMap_Map.prototype.initialize = function() {
+Scene_JumpToMap_Map.prototype.initialize = function () {
     Scene_Map.prototype.initialize.call(this);
     this._waitCount = 0;
     this._encounterEffectDuration = 0;
@@ -183,9 +239,9 @@ Scene_JumpToMap_Map.prototype.initialize = function() {
     this._touchCount = 0;
 };
 
-Scene_JumpToMap_Map.prototype.create = function() {
+Scene_JumpToMap_Map.prototype.create = function () {
     Scene_Base.prototype.create.call(this);
-	$gameSwitches.setValue(AXY.TitleMenu.Param.JumpToMapSwitchId, true);
+    $gameSwitches.setValue(AXY.TitleMenu.Value.CurrentSwitchId, true);
     this._transfer = $gamePlayer.isTransferring();
-    DataManager.loadMapData(AXY.TitleMenu.Param.JumpToMapId);
+    DataManager.loadMapData(AXY.TitleMenu.Value.CurrentMapId);
 };
